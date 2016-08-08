573ee3315890b9c12f8995e7f4d57b925ad59557
xingjingang <xingjingang@howzhi.com>
2016-08-08 17:29:16 +0800

    test

--- src/Topxia/WebBundle/Controller/KnowledgeControllerTest.php
+++ src/Topxia/WebBundle/Controller/KnowledgeControllerTest.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xingjingang
- * Date: 16/8/8
- * Time: 下午4:09
- */
+
+namespace Topxia\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class KnowledgeControllerTest extends WebTestCase
+{
+    function testDetail()
+    {
+        $client = static::createKernel();
+        $crawler = $client->request('GET', '/detail/1');
+        $this->assertTrue($crawler->filter('html:contains("今夜无人入睡")')->count() > 0);
+    }
+}
