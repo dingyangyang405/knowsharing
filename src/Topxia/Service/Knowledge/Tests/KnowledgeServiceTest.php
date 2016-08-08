a339b1b26e07f1b32543566968cb1b8d3a1f592c
dingyangyang <dingyangyang@howzhi.com>
2016-08-08 17:24:27 +0800

    添加只是的serviceTest

--- src/Topxia/Service/Knowledge/Tests/KnowledgeServiceTest.php
+++ src/Topxia/Service/Knowledge/Tests/KnowledgeServiceTest.php
@@ -6,8 +6,23 @@ use Topxia\Service\Knowledge\Impl\KnowledgeServiceImpl;
 
 class KnowledgeServiceTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFindKnowledges()
+    public function testAddKnowledge()
     {
-        $knowledgeServiceImpl = new KnowledgeServiceImpl(); 
+        $KnowledgeServiceImpl = new KnowledgeServiceImpl();
+        $data = array(
+            'title' => 'title',
+            'summary' => 'summary',
+            'content' => 'content',
+            'type' => 'file',
+            'userId' => 1,
+        );
+        $knowledge = $KnowledgeServiceImpl->addKnowledge($data);
+        $result = $KnowledgeServiceImpl->getKnowledgeDetial($data['id']);
+        
+        $this->assertEquals($knowledge['title'], $result['title']);
+        $this->assertEquals($knowledge['summary'], $result['summary']);
+        $this->assertEquals($knowledge['content'], $result['content']);
+        $this->assertEquals($knowledge['type'], $result['type']);
+        $this->assertEquals($knowledge['userId'], $result['userId']);
     }
 }
