6b58338e0ddb43351ce04ecde181ce300394dd4f
曹伟 <caowei@howzhi.com>
2016-08-09 16:49:49 +0800

    share

--- src/Topxia/Service/Knowledge/Dao/Impl/KnowledgeDaoImpl.php
+++ src/Topxia/Service/Knowledge/Dao/Impl/KnowledgeDaoImpl.php
@@ -15,7 +15,7 @@ class KnowledgeDaoImpl extends GeneralDaoImpl implements KnowledgeDao
         return $this->db()->fetchAll($sql);
     }
 
-    public function getKnowledgesByUserId($id)
+    public function findKnowledgeByUserId($id)
     {   
         $sql = "SELECT * FROM {$this->table()} WHERE userId = ?";
 
