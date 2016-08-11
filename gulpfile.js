99176dcd60884384ec252b09a5439c2a216d4e7a
xingjingang <xingjingang@howzhi.com>
2016-08-11 18:06:30 +0800

    style

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,23 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.styles([
         './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/select2/dist/css/select2.min.css',
         './app/Resources/public/css/AdminLTE.css',
         './app/Resources/public/css/style.css',
         './node_modules/admin-lte/dist/css/skins/skin-blue.css',
         './node_modules/font-awesome/css/font-awesome.css',
     ], 'web/assets/css/vendor.css');
 
-    // mix.styles([
-    //     './resources/assets/css/adminui.css',
-    // ], 'web/assets/css/adminui.css');
-
     mix.scripts([
         './node_modules/jquery/dist/jquery.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
         './node_modules/admin-lte/dist/js/app.js',
+        './node_modules/select2/dist/js/select2.min.js',
         './app/Resources/public/js/html5shiv.min.js',
     ], 'web/assets/js/vendor.js');
-
-    // mix.scripts([
-    //     './resources/assets/js/adminui.js',
-    // ], 'web/assets/js/adminui.js');
 
     mix.copy([
         './node_modules/font-awesome/fonts',
         './app/Resources/public/fonts',
     ],'web/assets/fonts');
-    // mix.copy('./resources/assets/js/pages', 'web/assets/js/pages');
 });
