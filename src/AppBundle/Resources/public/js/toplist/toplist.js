550f49b6285f9db2f34d2e20623f4ee9dbb536ad
江逸群 <jiangyiqun@howzhi.com>
2016-08-18 09:03:36 +0800

    fix

--- src/AppBundle/Resources/public/js/toplist/toplist.js
+++ src/AppBundle/Resources/public/js/toplist/toplist.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+    $("#like-knowledge").click(function(){
+        $.ajax({
+            url:$(this).data('url'),
+            data:{type:$(this).data('type')},
+            type:"POST",
+    	});
+    });
 
+    $("#favorite-knowledge").click(function(){
+        $.ajax({
+            url:$(this).data('url'),
+            data:{type:$(this).data('type')},
+            type:"POST",
+        });
+    });
+
+    $("#view-knowledge").click(function(){
+        $.ajax({
+            url:$(this).data('url'),
+            data:{type:$(this).data('type')},
+            type:"POST",
+        });
+    });
+});
