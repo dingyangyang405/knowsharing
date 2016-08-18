222687547606c7d37be111050d54ee658476d4f5
123456789 <zhangchen@howzhi.com>
2016-08-18 21:10:13 +0800

    bug

--- src/AppBundle/Resources/public/js/select2.js
+++ src/AppBundle/Resources/public/js/select2.js
@@ -40,6 +40,6 @@ function template(data) {
 
 $(".link-select").select2("data", $(".link-select").select2('data')[0]['id']);
 $('body').on('keyup','.select2-search__field',function() {
-    $(".link-select option ").val($(this).val());
+    $(this).parent().parent().parent().parent().find(".link-select option ").val($(this).val());
     $(this).parent().parent().parent().parent().find(".link-select").next().find('.selection').find('.select2-selection').find(".select2-selection__rendered").html($(this).val());
 });
