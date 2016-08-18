c2bc36763e592cca5ad58b4bc6f6f9f16523ca30
dingyangyang <dingyangyang@howzhi.com>
2016-08-18 10:17:12 +0800

    数据校验

--- src/AppBundle/Resources/public/js/modal.js
+++ src/AppBundle/Resources/public/js/modal.js
@@ -106,7 +106,7 @@ $(document).ready(function(){
         var fileSize = file.size;
         var maxSize = 20971520;
         if (fileSize >= maxSize) {
-            $("#title").val('文件不能大于20');
+            $("#title").val('文件不能大于20M');
             return;
         }
         $("#title").val(fileName);
