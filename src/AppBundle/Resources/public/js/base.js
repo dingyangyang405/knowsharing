c0454d4e1e546718bc3208a7df95ec68b8e90d5c
xingjingang <xingjingang@howzhi.com>
2016-08-21 21:43:36 +0800

    test

--- src/AppBundle/Resources/public/js/base.js
+++ src/AppBundle/Resources/public/js/base.js
@@ -11,4 +11,41 @@ $(function(){
     $(".gotop").click(function(){
         $('html,body').animate({'scrollTop':0},500);
     });
+    $("#header-xs-menu").find(".list-group-item").hide();
+    i = 0;
+    $(".header-xs-logo").click(function(){
+        i = i+1;
+        if(i%2 == 1 )
+        {
+            $("#header-xs-menu").show();
+        }else{
+            $("#header-xs-menu").hide();
+        }
+    });
+
+    k = 0;
+    $(".list-group-heading-first").click(function(){
+        k = k + 1;
+        if(k%2 == 1)
+        {
+            $("#header-xs-menu .list-group-item").hide();
+            z = 0;
+            $(this).parent().find(".list-group-item").show();
+        } else {
+            $("#header-xs-menu .list-group-item").hide();
+        }
+    });
+
+    z = 0;
+    $(".list-group-heading-second").click(function(){
+        z = z + 1;
+        if(z%2 == 1)
+        {
+            k = 0;
+            $("#header-xs-menu .list-group-item").hide();
+            $(this).parent().find(".list-group-item").show();
+        } else {
+            $("#header-xs-menu .list-group-item").hide();
+        }
+    });
 });
