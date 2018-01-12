e36436a09a661f72438a8084108dfc2de4b610d2
dingyangyang <dingyangyang@howzhi.com>
2018-01-12 16:44:29 +0800

    删除父类里面的getFailData方法

--- src/Message/BaseAbstractResponse.php
+++ src/Message/BaseAbstractResponse.php
@@ -20,12 +20,4 @@ abstract class BaseAbstractResponse extends \Omnipay\Common\Message\AbstractResp
         return isset($data['result_code']) && $data['result_code'] == 'SUCCESS';
     }
 
-    public function getFailData()
-    {
-        $data = $this->getData();
-        if ($data['result_code'] == 'FAIL') {
-            return '错误码：'.$data['err_code'].'，错误原因：'.$data['err_code_des'];
-        }
-        return null;
-    }
 }
--- src/Message/CloseOrderResponse.php
+++ src/Message/CloseOrderResponse.php
@@ -2,11 +2,41 @@
 
 namespace Omnipay\WechatPay\Message;
 
+use Omnipay\Common\Message\BaseAbstractResponse;
 /**
  * Class CloseOrderResponse
  * @package Omnipay\WechatPay\Message
  * @link    https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=9_3&index=5
  */
-class CloseOrderResponse extends \Omnipay\WechatPay\Message\BaseAbstractResponse
+class CloseOrderResponse extends BaseAbstractResponse
 {
+	public function isSuccessful()
+    {
+        return $this->isClosed();
+    }
+
+    public function isClosed()
+    {
+        $data = $this->getData();
+
+        if (!empty($data['req_info']['refund_status']) && $data['req_info']['refund_status'] == 'SUCCESS') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getFailData()
+    {
+        $data = $this->getData();
+        if ($data['result_code'] == 'FAIL') {
+            return '错误码：'.$data['err_code'].'，错误原因：'.$data['err_code_des'];
+        }
+        return null;
+    }
+
+    public function getRequestData()
+    {
+        return $this->request->getData();
+    }
 }
