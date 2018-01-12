89baeb17980dee5bd979abacf16be075e50dda8e
dingyangyang <dingyangyang@howzhi.com>
2018-01-12 17:26:13 +0800

    fix

--- src/Message/CloseOrderResponse.php
+++ src/Message/CloseOrderResponse.php
@@ -2,13 +2,12 @@
 
 namespace Omnipay\WechatPay\Message;
 
-use Omnipay\Common\Message\BaseAbstractResponse;
 /**
  * Class CloseOrderResponse
  * @package Omnipay\WechatPay\Message
  * @link    https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=9_3&index=5
  */
-class CloseOrderResponse extends BaseAbstractResponse
+class CloseOrderResponse extends \Omnipay\WechatPay\Message\BaseAbstractResponse
 {
 	public function isSuccessful()
     {
